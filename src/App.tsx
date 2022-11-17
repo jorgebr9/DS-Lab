ffb46365a383966f29a80dec222e471471187568
Jorge Carvalho <jorge.carvalho@focusnetworks.com.br>
2022-11-17 16:37:26 -0300

    adjust new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
+import { SignIn } from './pages/Signin';
+
 import './styles/global.css';
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl">hello world</h1>
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded-sm text-white hover:bg-cyan-300'>
-        Enviar
-      </button>
-    </>
+    <SignIn />
   )
 }
